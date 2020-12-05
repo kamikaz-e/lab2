e702ebc776b3bda6b29fa00c25dac8f7ac2588be
Michael Volkov <volkov@aeroidea.ru>
2020-12-05 13:48:01 +0300

    Fix interface bean in main

--- src/main/java/ru/ystu/lab2/Main.java
+++ src/main/java/ru/ystu/lab2/Main.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
-import ru.ystu.lab2.service.QueueServiceImpl;
+import ru.ystu.lab2.service.QueueService;
 
 @SpringBootApplication
 @ImportResource("classpath:beans.xml")
@@ -12,7 +12,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Main.class, args);
-		QueueServiceImpl service = context.getBean(QueueServiceImpl.class);
+		QueueService service = context.getBean(QueueService.class);
 		service.run();
 	}
 
